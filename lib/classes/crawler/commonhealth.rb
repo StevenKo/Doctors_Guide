a19c9c17b25ed7fa51cbfc311161e6f982f4eed4
Steven Ko <chunyuko85@gmail.com>
2015-10-02 13:43:22 +0800

    crawl commonhealth hospital

--- lib/classes/crawler/commonhealth.rb
+++ lib/classes/crawler/commonhealth.rb
@@ -15,4 +15,54 @@ class Crawler::Commonhealth
     end
   end
 
+  def crawl_detail hosp
+    lis = @page_html.css(".commonDrContentMain li")
+    grade = ""
+    lis.each do |li|
+      span = li.css("span")
+      if span.text == "評級 ："
+        li.css("span").remove
+        grade = li.text
+      end
+    end
+
+    time = {}
+
+    lis = @page_html.css("li#businessTimeLi ul > li")
+    lis.each do |li|
+      span_text = li.css("span").text
+      if span_text == "平日看診"
+        time_text = []
+        li.css("li").each do |li|
+          time_text.append(li.text)
+        end
+        time["weekday"] = time_text
+      end
+
+      if span_text == "夜間看診"
+        time_text = []
+        li.css("li").each do |li|
+          time_text.append(li.text)
+        end
+        time["night"] = time_text
+      end
+
+      if span_text == "假日看診"
+        time_text = []
+        li.css("li").each do |li|
+          time_text.append(li.text)
+        end
+        time["holiday"] = time_text
+      end
+
+      if li.attr('class') == "textLeft"
+        time["special"] = li.text
+      end
+    end
+    
+    hosp.cHours = time
+    hosp.grade = grade
+    hosp.save
+  end
+
 end
