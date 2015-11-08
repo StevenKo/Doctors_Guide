89918531502f04b66e486e640cea0a7c6b01d771
Steven Ko <chunyuko85@gmail.com>
2015-11-08 11:07:01 +0800

    fix businessweekly doc worker

--- app/workers/businessweekly_doc_worker.rb
+++ app/workers/businessweekly_doc_worker.rb
@@ -6,7 +6,7 @@ class BusinessweeklyDocWorker
     index_url = "http://health.businessweekly.com.tw/GSearchDoc.aspx?pro=0000&t=1&s=9&p="
     url = index_url + page.to_s
     c = Crawler::Businessweekly.new
-    c.fetch index_url
+    c.fetch url
     c.crawl_bw_doctors_url
   end
 end
